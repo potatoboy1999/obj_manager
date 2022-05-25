76ae1e5e15cfa253b8fcc446335e9e459d6db4c0
Alejandro <alejandrodazaculqui@hotmail.com>
2022-05-24 20:11:07 -0500

    new controllers with CRUD methods

--- app/Http/Controllers/MetricController.php
+++ app/Http/Controllers/MetricController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class TravelScheduleController extends Controller
+class MetricController extends Controller
 {
     /**
      * Display a listing of the resource.
